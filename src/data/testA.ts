bcbdc08c8f3555eba1331b06490f921f354dfc06
Abangpa1ace <dyek72731@gmail.com>
2022-03-27 18:24:31 +0900

    질문 데이터 추가중

--- src/data/testA.ts
+++ src/data/testA.ts
@@ -8,13 +8,93 @@ const testAList: TestList = [
     }
   },
   {
+    category: 'IE',
+    question: '질문?',
+    options: {
+      I: '답변1',
+      E: '답변2',
+    }
+  },
+  {
+    category: 'IE',
+    question: '질문?',
+    options: {
+      I: '답변1',
+      E: '답변2',
+    }
+  },
+  {
+    category: 'IE',
+    question: '질문?',
+    options: {
+      I: '답변1',
+      E: '답변2',
+    }
+  },
+  {
     category: 'SN',
     question: '질문?',
     options: {
       S: '답변1',
       N: '답변2',
     }
-  }
+  },
+  {
+    category: 'SN',
+    question: '질문?',
+    options: {
+      S: '답변1',
+      N: '답변2',
+    }
+  },
+  {
+    category: 'SN',
+    question: '질문?',
+    options: {
+      S: '답변1',
+      N: '답변2',
+    }
+  },
+  {
+    category: 'SN',
+    question: '질문?',
+    options: {
+      S: '답변1',
+      N: '답변2',
+    }
+  },
+  {
+    category: 'TF',
+    question: '질문?',
+    options: {
+      T: '답변1',
+      F: '답변2',
+    }
+  },
+  {
+    category: 'TF',
+    question: '질문?',
+    options: {
+      T: '답변1',
+      F: '답변2',
+    }
+  },
+  {
+    category: 'TF',
+    question: '질문?',
+    options: {
+      T: '답변1',
+      F: '답변2',
+    }
+  },
+  {
+    category: 'TF',
+    question: '질문?',
+    options: {
+      T: '답변1',
+      F: '답변2',
+    }
+  },
 ]
 
 export default testAList
