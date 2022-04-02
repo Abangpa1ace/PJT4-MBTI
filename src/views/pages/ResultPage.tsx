6e0f5f9b14867e640aac0225a582ca3f0a355a0d
Abangpa1ace <dyek72731@gmail.com>
2022-04-03 05:49:30 +0900

    테스트 적용중

--- src/views/pages/ResultPage.tsx
+++ src/views/pages/ResultPage.tsx
@@ -2,14 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 import BaseButton from '@/views/components/common/BaseButton';
-import Loader from '@/views/components/common/Loader';
+import useReactRouter from '@/hooks/useReactRouter';
 import s, { container } from '@/styles/mixin'
+import Loader from '@/views/components/common/Loader';
+import { useRecoilValue } from 'recoil';
+import { resultCodeB } from '@/recoil/main';
 
-const MidResultPage = () => {
+const MidResultPage: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [result, setResult] = useState<string>('');
+  const resultCode = useRecoilValue<string>(resultCodeB)
+  const { navigate } = useReactRouter()
 
   useEffect(() => {
     const loader = setTimeout(() => {
+      if (!resultCode) navigate('/mid-result')
+      setResult(resultCode);
       setLoading(false);
     }, 3000)
     return () => clearTimeout(loader)
