21ebf5f79fd82658884395a8e3cc7a7d1b84afc1
Abangpa1ace <dyek72731@gmail.com>
2022-05-15 20:50:38 +0900

    배포준비

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,10 +1,11 @@
-export const clip = (): void => {
-  let url = '';
-	const textarea = document.createElement("textarea");
-	document.body.appendChild(textarea);
-	url = window.document.location.href;
-	textarea.value = url;
-	textarea.select();
-	document.execCommand("copy");
-	document.body.removeChild(textarea);
+export const clip = () => {
+  // let url = '';
+	// const textarea = document.createElement("textarea");
+	// document.body.appendChild(textarea);
+	// url = window.document.location.href;
+	// textarea.value = url;
+	// textarea.select();
+	// document.execCommand("copy");
+	// document.body.removeChild(textarea);
+	navigator.clipboard.writeText(window.location.href);
 }
